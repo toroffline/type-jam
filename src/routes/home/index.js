0932e6b45d262e63c9d41330211b435ef0ccfecf
Narkkarux Tripiyaratana <narkkarux.t@gmail.com>
2022-06-08 20:26:09 +0700

    Initial scene render method

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,45 +1,75 @@
-import { h } from 'preact';
-import style from './style.css';
-import { useState, useEffect } from 'preact/hooks';
+import { h, createContext } from "preact";
+import style from "./style.css";
+import { useState, useContext, useEffect } from "preact/hooks";
 
-const player = {
-	mode: null
+const Scene = {
+  Mode_Selecting: "mode-selecting",
+  Play_Game: "play-game",
+};
+const Mode = {
+  Left_Hand_Only: "left-hand-only",
+  Both_Hand: "both-hand",
+  Right_Hand_Only: "right-hand-only",
 };
 
-const SceneSelectingMode = () => {
+const GameContext = createContext();
 
-	const selectMode = (mode) => {
-		player.mode = mode;
-	}
+function GameProvider(_props) {
+  const [mode, setMode] = useState();
+  const [scene, setScene] = useState(Scene.Mode_Selecting);
 
-	return <div>
-		<button onClick={() => selectMode('left')}>left</button>
-		<button>both</button>
-		<button>right</button>
-	</div>
+  useEffect(() => {
+    if (scene === Scene.Mode_Selecting && mode) {
+      setScene(Scene.Play_Game);
+    }
+  }, [scene, mode]);
+
+  return (
+    <GameContext.Provider value={{ mode, setMode, scene, setScene }}>
+      {_props.children}
+    </GameContext.Provider>
+  );
 }
 
+const SceneSelectingMode = () => {
+  const { setMode } = useContext(GameContext);
+
+  const selectMode = (mode) => setMode(mode);
+
+  return (
+    <div>
+      <button onClick={() => selectMode(Mode.Left_Hand_Only)}>left</button>
+      <button onClick={() => selectMode(Mode.Both_Hand)}>both</button>
+      <button onClick={() => selectMode(Mode.Right_Hand_Only)}>right</button>
+    </div>
+  );
+};
+
+const RenderScene = () => {
+  const { scene } = useContext(GameContext);
+
+  const render = () => {
+    switch (scene) {
+      case Scene.Mode_Selecting:
+        return <SceneSelectingMode />;
+      case Scene.Play_Game:
+        return <div>Play Game</div>;
+      default:
+        return <div>Something wrong</div>;
+    }
+  };
+
+  return render();
+};
+
 const Home = () => {
-	const sceneIdx = useState(1);
-
-	useEffect(() => {
-		console.log(player);
-	}, [player.mode])
-
-	const RenderScene = () => {
-		switch (sceneIdx) {
-			case 1:
-			default:
-				return <SceneSelectingMode />
-				break;
-		}
-		return <div>Something wrong</div>
-	}
-
-
-	return <div class={style.home}>
-			<RenderScene />
-		</div>
+  return (
+    <GameProvider>
+      <div class={style.home}>
+        <RenderScene />
+      </div>
+    </GameProvider>
+  );
 };
 
 export default Home;
