e5af72a31982d33dd7cb80755e0885db4916bdc1
DESKTOP-NBCPEOS\narkk <narkkarux.t@gmail.com>
2023-10-11 12:14:34 +0700

    Nui | I dont remember what I did

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,53 @@
-import { h } from 'preact';
-import style from './style.css';
+import { h } from "preact";
+import { useState, useEffect } from "preact/hooks";
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+const Home = () => {
+  const _words = ["note", "week", "quantity", "bride", "fail", "carry", "policy", "forum", "convenience", "past"];
+
+  const [words, setWords] = useState({ characters: [] });
+  const typing = useState("");
+  const currentIdx = useState(0);
+
+  function initial() {
+    const _chars = [];
+    let concat = "";
+    for (let word of _words) {
+      for (let i = 0; i < word.length; i++) {
+        const _char = {
+          character: word[i],
+          status: "wait",
+        };
+        _chars.push(_char);
+        concat += word[i];
+      }
+      concat += " ";
+    }
+
+    console.log(concat);
+
+    // const waitBoard = document.getElementById("wait");
+    // waitBoard.innerHTML = words.characters.map((w) => w.character).join(" ");
+  }
+
+  useEffect(() => {
+    initial();
+  }, []);
+
+  useEffect(() => {
+    if (typing) {
+    }
+  }, typing);
+
+  return (
+    <div className="home">
+      <div id="board" className="board">
+        <span id="done" className="done" />
+        <span id="current" className="current" />
+        <span id="wait" className="wait" />
+      </div>
+      <input id="typing-box" />
+    </div>
+  );
+};
 
 export default Home;
